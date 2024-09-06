fda788cdbf209a592f87db96c01f355e835845ee
Tamimi Ahmad <TamimiGitHub@users.noreply.github.com>
2024-09-06 10:56:48 -0700

    Update update_homebrew.sh
    URL Typo Update in homebrew installation setup

--- scripts/update_homebrew.sh
+++ scripts/update_homebrew.sh
@@ -13,7 +13,7 @@ cd homebrew-$FORMULA_NAME
 
 # Update formula
 pkg_version=$1
-sed -i "s|url .*|url \"https://github.com/$REPO_NAME/releases/download/$pkg_version/stm-macos-v$pkg_version.zip\"|" Formula/$FORMULA_NAME.rb
+sed -i "s|url .*|url \"https://github.com/$REPO_NAME/releases/download/v$pkg_version/stm-macos-v$pkg_version.zip\"|" Formula/$FORMULA_NAME.rb
 sed -i "s|sha256 .*|sha256 \"$(shasum -a 256 ../release/stm-macos-v$pkg_version.zip | awk '{ print $1 }')\"|" Formula/$FORMULA_NAME.rb
 # Commit and push changes
 git config --global user.email "community@solace.com"
